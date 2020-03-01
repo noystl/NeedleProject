bdfb77349849b456da38bf04a70c5163add5401d
noystl <noystll@gmail.com>
2020-03-01 16:25:38 +0200

    Implemented the methods in data_generator.py, removed the implementation of get_shingles from poc.py.

---
+++ slopes_poc/data_generator.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import math
+import OsmDataCollector as odc
+
+BAIERSBRONN_COORS = [8.1584, 48.4688, 8.4797, 48.6291]
+
+
+def generate_osm_track() -> pd.DataFrame:
+    """
+    Generates a pandas data frame (lat, lon) of an osm track. In the experiment, we'll try to
+    find the closest track to this one.
+    :return: pandas df as described above.
+    """
+    data_collector = odc.OsmDataCollector(BAIERSBRONN_COORS)
+    track = data_collector.tracks[0]
+    return track.extract_gps_points().iloc[:, :-1]
+
+
+def generate_test_tracks(osm_track: pd.DataFrame) -> list:
+    """
+    Generates a list of tracks that are different variations of osm_track.
+    For example: a cyclic shit of the original track, random permutation over it's points...
+    :param osm_track: the osm track chosen for the POC.
+    :return: a list of the generated tracks and the osm_track chosen for the test.
+    """
+    tracks = [osm_track]
+
+    t1 = osm_track.reindex(np.roll(osm_track.index, 30))  # Cyclic shift of 30 places upward.
+    tracks.append(t1)
+
+    t2 = osm_track.sample(frac=1).reset_index(drop=True)  # Random shuffle
+    tracks.append(t2)
+
+    t3 = osm_track.reindex(index=osm_track.index[::-1])  # Reverse
+    tracks.append(t3)
+
+    t4 = osm_track.apply(lambda x: x + 0.01 if x.name == 'lat' else x)  # Adding 0.01 to the latitude.
+    tracks.append(t4)
+
+    t5 = osm_track.apply(lambda x: x - 0.3 if x.name == 'lat' else x + 0.01)  # Sub 0.3 from lat, add 0.01 to lon.
+    tracks.append(t5)
+
+    return tracks
--- slopes_poc/poc.py
+++ slopes_poc/poc.py
@@ -1,7 +1,5 @@
-import math
 from datasketch import MinHash, MinHashLSH
-from slopes_poc import TestDataGenerator as genDat
-from geopy.distance import geodesic
+from slopes_poc import data_generator as genDat
 import pandas as pd
 
 """
@@ -9,37 +7,16 @@ This is our cool cool slopes comparision demo! In this POC we'll take a single O
 shingles vector, and use MinHash LSH to compare it efficiently with other tracks.
 """
 
-NUMBER_OF_SHINGLES = 100
 SHINGLE_LENGTH = 50  # just a random number, should be modified later.
 
 
-def get_shingles(points: pd.DataFrame) -> set:    # Todo: test this!
+def get_shingles(points: pd.DataFrame) -> set:  # Waiting for Bar's Implementation
     """
     Converts the given track into a set of shingles.
     :param points: a pandas df containing the lat lon of the points consisting a gps track.
     :return: a set of the slope-shingles appearing in the track.
     """
     shing_set = set()
-
-    def win_len(s, e):
-        return geodesic([points.lat[s], points.lon[s]], [points.lat[e], points.lon[e]]).m
-
-    def calc_slope(x1, x2, y1, y2):
-        return (x1 - x2) / (y1 - y2) if y1 != y2 else 0   # Todo: what are we suppose todo when the slope is undefined?
-
-    for i in range(len(points)):
-        win_start = i
-        win_end = win_start
-
-        while win_end < len(points) and win_len(win_start, win_end) < SHINGLE_LENGTH:
-            win_end += 1
-
-        if win_end >= len(points):
-            break
-
-        slope = calc_slope(points.lat[win_start], points.lat[win_end], points.lon[win_start], points.lon[win_end])  # todo: fix this. It doesn't calculte the right slope.
-        shingle = math.floor(slope * 100)
-        shing_set.add(shingle)
 
     return shing_set
 
@@ -56,20 +33,31 @@ def get_minhash(shingles: set) -> MinHash:
     return minhash
 
 
+def handle_track(key: str, points: pd.DataFrame, lsh_idx: MinHashLSH) -> MinHash:
+    """
+    Gets the track shingles, minhashes them and inserts the result into an LSH index
+    :param key: the key of this track in the LSH index.
+    :param points: a pandas df containing the track points (lat, lon).
+    :param lsh_idx: the minHashLSH index.
+    :return the minhash of the tracks's shingles
+    """
+    shingles = get_shingles(points)
+    min_hash = get_minhash(shingles)
+    lsh_idx.insert(key, min_hash)
+
+    return min_hash
+
+
 if __name__ == '__main__':
 
     osm_track = genDat.generate_osm_track()  # We want to find a similar track to this one.
-    test_tracks = genDat.generate_test_tracks()  # We will choose the most similar track from this set
+    test_tracks = genDat.generate_test_tracks(osm_track)  # We will choose the most similar track from this set
     lsh = MinHashLSH(threshold=0.7, num_perm=128)
 
     for idx, track_pts in enumerate(test_tracks):
-        shingles = get_shingles(track_pts)
-        min_hash = get_minhash(shingles)
-        lsh.insert(str(idx), min_hash)
+        handle_track(str(idx), track_pts, lsh)
 
-    shingles = get_shingles(osm_track)
-    osm_min_hash = get_minhash(shingles)
-    lsh.insert("osm_track", get_minhash(shingles))
-
+    osm_min_hash = handle_track('osm_track', osm_track, lsh)
     result = lsh.query(osm_min_hash)
+
     print("Approximate neighbours with Jaccard similarity > 0.7", result)
