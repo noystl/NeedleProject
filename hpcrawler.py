7a816c71083ec3f2e80213657d41b7e6f571d6ba
bar <bar.aloni@mail.huji.ac.il>
2020-03-04 01:37:49 +0200

    done. * need to decide about crawling all tails in a certain country in a way of "all or nothing" - more in the 1st TODO. * can be used for the crawling task.

--- hpcrawler.py
+++ hpcrawler.py
@@ -1,3 +1,19 @@
+# TERMINOLOGY EXPLAINED:
+# 1. every state has a code <i>. (sometimes referred to as index)
+# 2. we save the gpx tracks in a directory named "<i>", inside of the directory GPX_DIR_PATH
+# 3. we save the tracks in dicts by length, and the dicts we save to files:
+#   (i) each dict is saved to a json file inside of the directory TRACKS_DIR_PATH under the name
+#       "C<i>_L<l>", where <l> represents the lengths range of the tracks in the dict cultivated in the state <i>.
+#   (ii) the keys of the tracks in a dict are: "C<i>_T<j>", where <j> is the number of the track cultivated
+#       in the state <i>.
+
+# TODO's: 1. don't delete and re-download gpx files of a country if we didn't finished,
+#            although the program can crash and we'll lose the site's features?
+#         2. create Enum for difficulty levels?
+#         3. can we use the similarity features to decide the rank of the trail? :)
+#         4. doesn't know 'AUSTRALIA', 'daef#' -  what to do?
+
+import shutil
 from selenium import webdriver
 import json
 import os
@@ -6,15 +22,15 @@ import pandas as pd
 import slopeMap as sm
 import numpy as np
 
-GPX_DIR_PATH = 'hp_gpx'
-TRACKS_DIR_PATH = 'hp_tracks'
+GPX_DIR_PATH = 'hp\\hp_gpx'
+TRACKS_DIR_PATH = 'hp\\hp_tracks'
 
-MAX_TRACK_LEN = 30
+MAX_TRACK_LEN = 31
 LEN_SPACING = 5
-TICK = 0.25
+TICK = 25
 
 
-def setup():
+def setup(index):
     """
     creates a firefox driver which is capable of downloading files without popups
     """
@@ -25,8 +41,12 @@ def setup():
 
     if not os.path.exists(GPX_DIR_PATH):
         os.makedirs(GPX_DIR_PATH)
+    i_path = os.path.join(GPX_DIR_PATH, str(index))
+    if os.path.exists(i_path):
+        shutil.rmtree(i_path, ignore_errors=True)
+    os.makedirs(i_path)
 
-    profile.set_preference('browser.download.dir', os.path.abspath(GPX_DIR_PATH))
+    profile.set_preference('browser.download.dir', os.path.abspath(i_path))
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
     driver = webdriver.Firefox(firefox_profile=profile)
     return driver
@@ -35,6 +55,7 @@ def setup():
 def log_in(driver):
     """
     logs into hikingproject.com
+    :param driver: firefox driver
     """
     print("log in")
     homepage(driver)
@@ -55,13 +76,19 @@ def log_in(driver):
 
 
 def homepage(driver):
-    print("homepage")
+    """
+    navugates the the hiking project's homepage
+    :param driver: firefox driver
+    """
     driver.get("https://www.hikingproject.com/")
 
 
 def trails_in_urls(driver, name):
     """
-    return a list of urls (strings) for paths in <name> (example Israel, Italy, France...)
+    return a list of urls (strings) for paths in a country named <name>
+    :param driver: firefox driver
+    :param name: country's name.
+
     """
     homepage(driver)
     query = "//a[@title='" + name + "']"
@@ -98,36 +125,51 @@ def get_page_data(driver):
     e = driver.find_elements_by_xpath("//div[@class='stat-block mx-1 pb-2']")[0]
     txt = e.text
     txt = txt.split()
-    track_len = txt[1]
+    track_len = float(txt[1])
     track_dif = txt[-1]
     return track_len, track_dif
 
 
-def getTick(length):
+def get_tick(track_len):
     """
     returns the tick according to the track's length supplied.
-    :param length: a track's length (km)
-    :return: in
+    the tag is computed from the formula:
+    for int i in [0,
+    return [LEN_SPACING * i, LEN_SPACING * (i + 1)] =  TICK * (i + 1) / 100
+    :param track_len: a track's length (km)
+    :return: double (km)
     """
     ranges = np.arange(0, MAX_TRACK_LEN, LEN_SPACING)
-    for i in length(ranges) - 1:
-        if ranges[i] < length <= ranges[i-1]:
-            return TICK * (i + 1)
+    for i in range(len(ranges) - 1):
+        if ranges[i] < track_len <= ranges[i+1]:
+            return TICK * (i + 1) / 100
 
 
-def get_length_tag(trackLength):
-    if trackLength % LEN_SPACING == 0:
-        return trackLength // LEN_SPACING - 1
-    return trackLength // LEN_SPACING
+def get_length_tag(track_len):
+    """
+    :param track_len: int representing track's length in km
+    :return: an int representing the length tag of the given  track's length
+    """
+    if track_len % LEN_SPACING == 0:
+        return track_len // LEN_SPACING - 1
+    return int(track_len // LEN_SPACING)
 
 
 def save_data(data, index):
+    """
+    saves rhe data's inner dicts to a json file inside of the directory TRACKS_DIR_PATH under the name
+    "<i>_<l>", where <l> represents the lengths range of the tracks in the dict cultivated in the state <i>.
+    :param data: python list of dicts, each dict holds tracks of length tag  <k>.
+    :param index: country idx
+    """
     if not os.path.exists(TRACKS_DIR_PATH):
         os.makedirs(TRACKS_DIR_PATH)
-    for i in np.arange(MAX_TRACK_LEN // LEN_SPACING):
-        path = os.path.join(TRACKS_DIR_PATH, 'L' + i + '_' + str(index) + '.json')
-        with open(path, 'w') as f:
-            json.dump(data[i], f)
+
+    for l in range(MAX_TRACK_LEN // LEN_SPACING):
+        if data[l]:  # if the dict isn't empty (... we'll save it)
+            path = os.path.join(TRACKS_DIR_PATH, 'C' + str(index) + '_L' + str(l) + '.json')
+            with open(path, 'w') as f:
+                json.dump(data[l], f)
 
 
 def collect_tracks_data(driver, urls, index):
@@ -138,23 +180,25 @@ def collect_tracks_data(driver, urls, index):
     :param index: number of state
     """
     features = {}
+    print("urls: ", len(urls))
 
-    # saves gpx files:
-    files_so_far = len([name for name in os.listdir(GPX_DIR_PATH) if os.path.isfile(name)])  # TODO
-    for i in np.arange(files_so_far, len(urls) + 1):
-        driver.get(urls[i])
+    # for j in range(1):  # TODO - for testing
+    for j in range(len(urls)):
+        driver.get(urls[j])
 
         # get track's length and difficulty from site:
         track_len, track_dif = get_page_data(driver)
 
         # download gpx file:
-        before = os.listdir('/home/jason/Downloads')
+        before = os.listdir(os.path.join(GPX_DIR_PATH, str(index)))
         driver.find_element_by_link_text("GPX File").click()
-        after = os.listdir('/home/jason/Downloads')
+        after = os.listdir(os.path.join(GPX_DIR_PATH, str(index)))
         change = set(after) - set(before)
         filename = change.pop()
 
-        features[i] = [filename, track_len, track_dif]
+        features[j] = [filename, track_len, track_dif]
+
+        print("f", j, ": ", filename)
     return features
 
 
@@ -162,47 +206,92 @@ def process_tracks_data(features, index):
     data = [dict() for x in range(MAX_TRACK_LEN // LEN_SPACING)]
     points, trackElev = pd.DataFrame({}), pd.DataFrame({})
 
-    for i in range(len(features)):
-        filename, track_len, track_dif = features[i]
+    for j in range(len(features)):
+        filename, track_len, track_dif = features[j]
 
         # open gpx file:
-        gpx_file = open(os.path.join(GPX_DIR_PATH, filename), 'r', encoding="utf8")
+        gpx_file = open(os.path.join(GPX_DIR_PATH, str(index), filename), 'r', encoding="utf8")
         gpx = gpxpy.parse(gpx_file)
 
         # get points array & elevations:
         for track in gpx.tracks:
             for seg in track.segments:
-                points = pd.DataFrame([{'lat': p.latitude, 'lon': p.longitude, 'time': p.time} for p in seg.points])
-                trackElev = pd.DataFrame([{'ele': p.latitude} for p in seg.points])
+                points = pd.DataFrame([{'lat': p.latitude, 'lon': p.longitude} for p in seg.points]).to_numpy()
+                trackElev = pd.DataFrame([{'ele': p.elevation} for p in seg.points]).to_numpy().reshape(len(points))
 
         # compute slopes:
-        tick = getTick(track_len)
+        tick = get_tick(track_len)
         slopes = sm.computeSlope(points, trackElev, tick)
 
-        # save track to the correct dict (according to it's length):
+        # save track to the correct dict (according to the track's length):
         len_tag = get_length_tag(track_len)
-        data[len_tag][str(i) + '_' + str(index)] = [slopes, track_dif]
+
+        data[len_tag]['C' + str(index) + '_T' + str(j)] = [slopes, track_dif]
     save_data(data, index)
+
+
+def runs(countries, seen):
+    """
+    runs the hp data crawling and data processing, for the countries supplied in <countries>,
+    excluding the ones that appear in <seen>.
+    :param countries: python list of countries whom trails we want to download
+    :param seen: a dictionary of the countries we've already processed (to prevent double crawling)
+    :return: a dictionary of the new countries from <countries> (that are not in <seen>),
+            we've processed ib this pass. the keys are strings (country name as given in <countries>),
+            the values are the country id (int).
+    """
+    index = len(seen.keys()) - 1
+    this_pass = {}
+
+    for i in range(len(countries)):
+
+        if countries[i] in seen:  # prevent processing the countries we've already processed
+            continue
+
+        index += 1  # new country!
+        print("\n", countries[i])
+
+        # create gpx folder for country of index i, sets up driver to website:
+        ff_driver = setup(index)
+        try:  # TODO: catch-> try again
+            log_in(ff_driver)
+        except:
+            log_in(ff_driver)
+
+        # collects all urls of country <i>'s tracks:
+        trail_urls = trails_in_urls(ff_driver, countries[i])
+
+        # collects the data from web, and downloads the gpx files:
+        fs = collect_tracks_data(ff_driver, trail_urls, index)
+
+        # process the data from web and from the gpx to create a representation of
+        # our liking and saves it under a folder it creates for country <i>:
+        process_tracks_data(fs, index)
+
+        this_pass[countries[i]] = index
+    return this_pass
 
 
 if __name__ == "__main__":
     # countries crawled so far - Australia, Brazil, France, Italy, Switzerland, South Africa, United Kingdom
-    # ,'Alaska', 'Alabama', 'Illinois', 'Florida', 'Ohio', 'Rhode Island', 'Vermont'
-    countries = ['Australia']
-    ff_driver = setup()     # create gpx folder, sets up driver
-    log_in(ff_driver)
+    # ,'Alaska', 'Alabama', 'Illinois', 'Florida', 'Ohio', 'Rhode Island', 'Vermont']
 
-    for i in range(len(countries)):
-        print(countries[i])
-        trail_urls = trails_in_urls(ff_driver, countries[i])
-        fs = collect_tracks_data(ff_driver, trail_urls, i)
-        process_tracks_data(fs, i)
+    to_crawl = ['Australia', 'Brazil', 'France', 'Italy', 'Switzerland', 'South Africa', 'United Kingdom',
+                'Alaska', 'Alabama', 'Illinois', 'Florida', 'Ohio', 'Rhode Island', 'Vermont']
 
-    # TERMINOLOGY EXPLAINED:
-    # every state has a code <i>.
-    # in collect_data_by_url_list we save the tracks in dicts by length, and the dicts we save to files:
-    # each dict is saved to a file under the name: "L<k>_<i>.json", where <k> represents the lengths
-    # range of the tracks in the dict, cultivated in the state <i>.
-    # the keys of the tracks in a dict are: "<i>_<j>". it is a string where <j> is the number of the
-    # track cultivated in the state <i>.
+    # read back the dict holding the countries we've already processed:
+    prev_seen = {}
+    if os.path.exists("hp\\seen.json"):
+        with open("hp\\seen.json", "r") as f:
+            prev_seen = f.read()
+        prev_seen = json.loads(prev_seen)
 
+    this_pass = runs(to_crawl, prev_seen)
+
+    # write back the dictionary now containing the previously processed countries
+    # and the countries we've processed in this pass:
+    prev_seen.update(this_pass)
+    json = json.dumps(prev_seen)
+    f = open("hp\\seen.json", "w")
+    f.write(json)
+    f.close()
