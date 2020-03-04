8d90bede7dd07b9cb8ebd33e798071e6ae3e5c3c
bar <bar.aloni@mail.huji.ac.il>
2020-03-04 12:57:01 +0200

    program's layout

---
+++ program.py
@@ -0,0 +1,27 @@
+
+import hpcrawler
+
+
+def main():
+
+    # countries crawled so far - Australia, Brazil, France, Italy, Switzerland, South Africa, United Kingdom
+    # ,'Alaska', 'Alabama', 'Illinois', 'Florida', 'Ohio', 'Rhode Island', 'Vermont']
+
+    to_crawl = ['Australia', 'Brazil', 'France', 'Italy', 'Switzerland', 'South Africa', 'United Kingdom',
+                'Alaska', 'Alabama', 'Illinois', 'Florida', 'Ohio', 'Rhode Island', 'Vermont']
+
+    # (1) PREPARE DATA:
+
+    # get hp data-
+    hpcrawler.HpCrawler(to_crawl)
+    # get osm data (completed from hp data)-
+
+    # (2) SUPPORT USER QUERIES:
+
+    # get query-
+
+    # get response to query-
+
+    # present response to user-
+
+
