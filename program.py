801986088f095d4f586136ad35db9649074cf92c
bar <bar.aloni@mail.huji.ac.il>
2020-03-07 04:23:06 +0200

    contains the implementation for hp mining

--- program.py
+++ program.py
@@ -1,38 +1,46 @@
 import hpcrawler
 
 
-def main(to_crawl):
-    # (1) PREPARE DATA:
-
-    # get hp data-
-    seen = {}
-    while seen.keys() != set(to_crawl):
+def create_hp_data_set(to_crawl):
+    # For The program:
+    seen_countries = []
+    while not set(to_crawl).issubset(set(seen_countries)):
+        hpc = hpcrawler.HpCrawler(to_crawl)
         try:
-            hpcrawler.HpCrawler(to_crawl)
+            hpc.crawl()
+            seen_countries = hpcrawler.HpCrawler.load_seen().keys()
         except Exception as e:
-            print("\n", str(e), "\nstarting again..\n")
             seen = hpcrawler.HpCrawler.load_seen()
-            print(seen)
+            del hpc
+            print("\n", str(e), "\ninner state: ", seen, "\nstarting again..\n")
             continue
 
+    # # For DEBUGGING:
     # hpcrawler.HpCrawler(to_crawl)
     # seen = hpcrawler.HpCrawler.load_seen()
     # print(seen)
 
-    # get osm data (completed from hp data)-
+
+def create_osm_data_set():
+    pass
+
+
+def main(to_crawl):
+    # (1) PREPARE DATA:
+    create_hp_data_set(to_crawl)
+    create_osm_data_set()  # based on hp data set
 
     # (2) SUPPORT USER QUERIES:
-
     stay = 1
     while stay:
 
-    # get query-
+        # get query-
 
-    # get response to query-
+        # get response to query-
 
-    # present response to user-
+        # present response to user-
 
-    # end-
+        # end-
 
         stay = 0
 
@@ -44,5 +52,5 @@ if __name__ == '__main__':
     # to_crawl = ['Australia', 'Brazil', 'France', 'Italy', 'Switzerland', 'South Africa', 'United Kingdom',
     #
 
-    to_crawl = ['Brazil', 'Australia', 'France', 'Italy', 'Switzerland']
+    to_crawl = ['Israel']
     main(to_crawl)
