90a06c0712aeb5cfa8bda05615bec24e9458af99
noystl <noystll@gmail.com>
2020-03-14 09:46:35 +0200

    Adjusted the module to work with hp elevations instead of translating the hp gpx files into OSM objects and using tiles.

--- Evaluation/eval_diffuculty.py
+++ Evaluation/eval_diffuculty.py
@@ -1,4 +1,3 @@
-import math
 from sklearn import metrics
 from Evaluation import eval_util
 from EvaluateDifficulty import DifficultyEvaluator
@@ -10,20 +9,25 @@ TEST_TILES_PATH = 'test_tiles\\'
 def get_model_predictions(tracks: list, neighbors=5) -> list:
     """
     Gets the predictions of the models for the given tracks with the given number of neighbors.
-    :param tracks: a list of OsmTrack objects
+    :param tracks: a list of pandas df of the form (lat, lon, time, elev) representing an hp tracks with known
+    heights.
     :param neighbors: the number of neighbors the model is using (in KNN) to evaluate the difficulty of the
     given tracks.
-    :return: a list of the predicted difficulty levels of the givien tracks.
+    :return: a list of the predicted difficulty levels of the given tracks.
     """
     predictions = []
-    for track in tracks:
-        track_corner = [math.floor(track.boundaries['south']), math.floor(track.boundaries['east'])]
-        tile_name = 'S' + str(abs(track_corner[0])) + 'E' + str(abs(track_corner[1]))
-        diff_evaluator = DifficultyEvaluator(TEST_TILES_PATH + tile_name + '.hgt',
-                                             track_corner,
+    for track in tracks:                                                                # Todo: fix this bad code.
+        diff_evaluator = DifficultyEvaluator(TEST_TILES_PATH + 'N14E120' + '.hgt',
+                                             [14, 120],
                                              1)
-        diff_evaluator.add_difficulty(track)
-        predictions.append(track.difficulty.value)
+        difficulty = diff_evaluator.pred_difficulty_known_heights(track, neighbors)
+        if difficulty == 'Easy':
+            predictions.append(TrackDifficulty.EASY.value)
+        elif difficulty == 'Intermediate':
+            predictions.append(TrackDifficulty.INTERMEDIATE.value)
+        else:
+            predictions.append(TrackDifficulty.DIFFICULT.value)
+    # print('pred ' + str(predictions))
     return predictions
 
 
@@ -35,16 +39,19 @@ def eval_difficulty():
     exp_data.update(exp_data['real'].str.lower())
     exp_data = exp_data.replace('very difficult', TrackDifficulty.DIFFICULT.value)
 
-    tracks = [eval_util.convert_to_osm(exp_data.gpx[i], i) for i in range(len(exp_data))]
+    tracks = [eval_util.read_track_to_df(row.gpx) for idx, row in exp_data.iterrows()]
+
     labels = [TrackDifficulty.EASY.value, TrackDifficulty.INTERMEDIATE.value, TrackDifficulty.DIFFICULT.value]
     results = {'accuracy': [], 'precision': [], 'recall': []}
 
-    for thresh in range(1, 2):
-        predictions = get_model_predictions(tracks, thresh)
+    for k in range(1, 30):
+        print(k)
+        predictions = get_model_predictions(tracks, k)
         real = exp_data['real'].values.tolist()
+        # print('real' + str(real))
         results['accuracy'].append(metrics.accuracy_score(real, predictions))
-        results['precision'].append(metrics.precision_score(real, predictions, labels=labels, average='micro'))
-        results['recall'].append(metrics.recall_score(real, predictions, labels=labels, average='micro'))
+        results['precision'].append(metrics.precision_score(real, predictions, labels=labels, average='weighted'))
+        results['recall'].append(metrics.recall_score(real, predictions, labels=labels, average='weighted'))
     return results
 
 
