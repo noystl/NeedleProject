369ab4e618e5cb4e28f41d36589d19653601dfb0
bar <bar.aloni@mail.huji.ac.il>
2020-03-15 22:00:12 +0200

    ready

--- slopeMap.py
+++ slopeMap.py
@@ -3,22 +3,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from geopy.distance import distance
-import OsmDataCollector as odc
-import locale
 
-areas_paths = {'baiersbronn': ['N48E008.hgt', [48, 8]]}  # Other areas in the future :)
-
-MAX_TRACK_LEN = 10001  # maximum supported track length - this parameter is obsolete
 LEN_SPACING = 5  # size of the "buckets" of the length_tag
 TICK = 0.125  # in kms
-DEG_GENERALIZE = 15  # size of "buckets for slopes, in degrees
-
-
-def get_num_of_len_tags():  # this function is obsolete
-    """
-    :return: the number of different categories of length.
-    """
-    return MAX_TRACK_LEN // LEN_SPACING
 
 
 def get_tick(track_len):
@@ -27,13 +14,9 @@ def get_tick(track_len):
     :param track_len: a track's length (km)
     :return: double (km)
     """
-    #ranges = np.arange(0, MAX_TRACK_LEN, LEN_SPACING)
     len_tag = get_length_tag(track_len)
     return TICK * (len_tag + 1)
-    # the below implementation is wasteful (and makes the ticks slightly larger than necessary)
-    # for i in range(len(ranges) - 1):
-    #    if ranges[i] >= track_len:
-    #        return (TICK * 100) * (i + 1) / 100
+
 
 def get_length_tag(track_len):
     """
@@ -137,15 +120,12 @@ def plot_dist_elevation(kms, elevations):
 
 
 # slope Representation #
-def compute_slope(track_points, track_elevs, track_length):  # TODO: notify about the signature change
+def compute_slope(track_points, track_elevs, track_length):
     """
-    returns a python list of
     :param track_points: 2-dim np array of the track's points: (lat, lon).
-    :param: track_length: float
-    :param track_elevs: np array of length n, holding the elevations at points.
-    :return: python list of floats representing the angles (values are in [-90, 90],
-            all <DEG_GENERALIZE> degrees subsections are mapped to one value representing
-            the slope of that subsection)
+    :param track_elevs: float
+    :param track_length: np array of length n, holding the elevations at points.
+    :return: python list of floats representing the track's angles (values are in [-90, 90])
     """
     track_kms = compute_track_km(track_points)
     tick = get_tick(track_length)
@@ -155,59 +135,11 @@ def compute_slope(track_points, track_elevs, track_length):  # TODO: notify abou
     if track_kms[-1] % tick == 0:
         np.append(km_marks, track_kms[-1])
 
-    # alternatively: add the last km value of the original track
-    # (NOTE: the last kmMark will probably not be a multiple of tick)
-    #  ADD THIS CODE:
-    #   if trackKms[-1] > kmMarks[-1]:
-    #   np.append(kmMarks, trackKms[-1])
-
     # interpolate the elevation values at kmMarks:
     elev_marks = np.interp(km_marks, track_kms, track_elevs)
-
-    # TODO: FOR VISUALIZING-
-    #plot_dist_elevation(km_marks, elev_marks)
 
     # get slopes of all sections:
     slopes = (elev_marks[1:] - elev_marks[:-1]) / tick  # slope between all 2 following tick points
     slopes = [math.degrees(rad) for rad in np.arctan(slopes)]  # the slope in degrees
 
     return slopes
-
-
-def slopes_sanity_check():
-    kms = np.array([0, 1, 2, 3, 4, 5])
-    elevs = np.array([0, 1, 2, 0, 0, 0])
-    slopes = (elevs[2:] - elevs[:-2]) / 2
-    slopes = [math.degrees(rad) for rad in np.arctan(slopes)]
-
-    fig, ax = plt.subplots()
-    ax.plot(kms, elevs)
-    ax.scatter(kms, elevs, color='r', s=10, edgecolors='red')
-    # label axis:
-    plt.xlabel('Distance (km)')
-    plt.ylabel('Elevation (meters)')
-    plt.show()
-    return slopes
-
-
-if __name__ == "__main__":
-    # # get track gps points (lat, lon):
-    elev_map_path, tile_rep = areas_paths['baiersbronn']
-    BAIERSBRONN_COORS = [8.1584, 48.4688, 8.4797, 48.6291]
-
-    data_collector = odc.OsmDataCollector(BAIERSBRONN_COORS)  # TODO Noy add param to set the number of files we want to extract
-    track = data_collector.tracks[0]
-    points = track.extract_gps_points()
-    points = points.to_numpy()[:, :2]
-    elevs = compute_track_elevation(elev_map_path, tile_rep, points)
-
-    # # compute slopes, plot the change in elevation(in meters) over distance (in km). marking kmMark points
-    # # according to the spacing:
-    slopes = compute_slope(points, elevs, track.length)  # TODO Noy add length getter in OSM
-    print(slopes)
-
-    # Slopes sanity check:
-    # slopes slopes_sanity_check()
-    # print(slopes)
-
-
